d9cb291a254d44194243ae8de46e9dac415c8537
gnidinger <gcnb87@gmail.com>
2022-08-14 18:44:28 +0900

    9498번

--- src/Prob9498.java
+++ src/Prob9498.java
@@ -10,6 +10,6 @@ public class Prob9498 {
         else if(80 <= a && a <= 89) System.out.println('B');
         else if(70 <= a && a <= 79) System.out.println('C');
         else if(60 <= a && a <= 69) System.out.println('D');
-        else System.out.print('F');
+        else System.out.println('F');
     }
 }

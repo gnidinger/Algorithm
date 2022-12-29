0098db5830f8178d3d54fc4d784253886f6b7bc0
gnidinger <gcnb87@gmail.com>
2022-12-29 10:10:26 +0900

    4673번

---
+++ src/BackJoon/Prob4673Stream.java
@@ -0,0 +1,33 @@
+package BackJoon;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Prob4673Stream {
+    public static void main(String[] args) {
+
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+
+        for (int i = 1; i <= 10000; i++) {
+            list1.add(i);
+        }
+
+        for (int i = 1; i <= 10000; i++) {
+            list2.add(i +
+                    i % 10 +
+                    (i % 100) / 10 +
+                    (i % 1000) / 100 +
+                    (i % 10000) / 1000);
+        }
+
+        list1.stream()
+                .filter(a -> list2.stream()
+                        .distinct()
+                        .filter(b -> b <= 10000)
+                        .noneMatch(Predicate.isEqual(a)))
+                .sorted()
+                .forEach(System.out::println);
+    }
+}

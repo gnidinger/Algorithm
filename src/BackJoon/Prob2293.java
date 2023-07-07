87fd158b6278b05ba4503ff50dc66b85721401df
gnidinger <gcnb87@gmail.com>
2023-07-08 00:15:29 +0900

    Commit

---
+++ src/BackJoon/Prob2293.java
@@ -0,0 +1,34 @@
+package BackJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Prob2293 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		int[] coins = new int[n];
+		int[] dp = new int[k + 1];
+
+		for (int i = 0; i < n; i++) {
+			coins[i] = Integer.parseInt(br.readLine());
+		}
+
+		dp[0] = 1;
+
+		for (int i = 0; i < n; i++) {
+			for (int j = coins[i]; j <= k; j++) {
+				dp[j] += dp[j - coins[i]];
+			}
+		}
+
+		System.out.println(dp[k]);
+	}
+}

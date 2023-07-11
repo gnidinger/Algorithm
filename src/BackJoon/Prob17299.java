396867f8677f36381011f8aa946824ad067f7a1d
gnidinger <gcnb87@gmail.com>
2023-07-11 12:32:01 +0900

    Commit

--- src/BackJoon/Prob17299.java
+++ src/BackJoon/Prob17299.java
@@ -7,7 +7,7 @@ import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Prob17298 {
+public class Prob17299 {
 
 	public static void main(String[] args) throws IOException {
 
@@ -15,27 +15,29 @@ public class Prob17298 {
 
 		int n = Integer.parseInt(br.readLine());
 		int[] arr = new int[n];
+		int[] freq = new int[1000001];
 		int[] result = new int[n];
 		Arrays.fill(result, -1);
+
+		ArrayDeque<Integer> stack = new ArrayDeque<>();
 
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		for (int i = 0; i < n; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
+			freq[arr[i]]++;
 		}
 
-		ArrayDeque<Integer> stack = new ArrayDeque<>();
 		stack.push(0);
-
 		for (int i = 1; i < n; i++) {
-			while (!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
+			while (!stack.isEmpty() && freq[arr[stack.peek()]] < freq[arr[i]]) {
 				result[stack.pop()] = arr[i];
 			}
 			stack.push(i);
 		}
 
 		StringBuilder sb = new StringBuilder();
-		for (int i : result) {
-			sb.append(i).append(' ');
+		for (int i = 0; i < n; i++) {
+			sb.append(result[i]).append(' ');
 		}
 
 		System.out.println(sb);

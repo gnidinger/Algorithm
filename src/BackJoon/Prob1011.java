748d58ef104eb6455065270500acce1aa1530cb2
gnidinger <gnidinger@gmail.com>
2023-09-01 09:48:32 +0900

    commit

---
+++ src/BackJoon/Prob1011.java
@@ -0,0 +1,33 @@
+package BackJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Prob1011 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < t; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			long x = Long.parseLong(st.nextToken());
+			long y = Long.parseLong(st.nextToken());
+			long distance = y - x;
+
+			long k = 1;
+			while (k * k < distance) {
+				k++;
+			}
+
+			if (k * k == distance) {
+				System.out.println(2 * k - 1);
+			} else if (k * k - k < distance && distance < k * k) {
+				System.out.println(2 * k - 1);
+			} else {
+				System.out.println(2 * k - 2);
+			}
+		}
+	}
+}
